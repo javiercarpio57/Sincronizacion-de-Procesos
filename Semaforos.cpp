4e035102d13ec764c0e06be753d4abf19e6c6876
javiercarpio57 <javierrr.carpio57@gmail.com>
2020-04-01 17:09:41 -0600

    Programa inicial

---
+++ Semaforos.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+#define threadsNum 2;
+#define ciclosNum 5;
+
+int main() {
+    cout << "Hello World!";
+
+    cout << "El numero de threads es: " << threadsNum << endl;
+    cout << "El numero de ciclos es: " << ciclosNum << endl;
+    return 0;
+}
