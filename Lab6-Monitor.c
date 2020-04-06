06c8a72e87cebe70d0e0fc99a17dc64821bc0d2e
Javier Carpio <javierrr.carpio57@gmail.com>
2020-04-06 17:45:07 -0600

    Semaforo listo y avances de monitor

--- Lab6-Monitor.c
+++ Lab6-Monitor.c
@@ -1,5 +1,6 @@
 # include <unistd.h>
 # include <stdlib.h>
+# include <sys/syscall.h>
 # include <stdio.h>
 # include <pthread.h>
 # include <semaphore.h>
@@ -17,6 +18,104 @@ struct Monitor {
 };
 
 struct Monitor monitor;
+sem_t x_sem;
+int x_count = 0;
+
+// x.wait()
+void wait () {
+    x_count++;
+
+    if (monitor.next_count > 0)
+        sem_post(&monitor.next);
+    else
+        sem_post(&monitor.mutex);
+
+    int status = sem_wait(&x_sem);
+    printf("STATUS: %d\n", status);
+    x_count--;
+}
+
+// x.signal()
+void signal () {
+    if (x_count > 0) {
+        monitor.next_count++;
+        sem_post (&x_sem);
+        sem_wait (&monitor.next);
+        monitor.next_count--;
+    }
+}
+
+int initialize_monitor () {
+    int return_value = 1;
+
+    monitor.available_resources = maxRecursos;
+    sem_init(&x_sem, 0, 1);
+
+    if ((sem_init(&(monitor.mutex), 0, 1) == 0) && (sem_init(&(monitor.next), 0, 0) == 0))
+        return_value = 0;
+    else
+        printf("Unable to initialize semaphores\n");
+
+    return return_value;
+}
+
+void* decrease_count (int count) {
+    pid_t tid = syscall(SYS_gettid);
+
+    printf("\t(%d) Iniciando decrease_count.\n", tid);
+
+    sem_wait(&(monitor.mutex));
+    printf("\tMutex adquirido, entrando a monitor.\n");
+
+    if (monitor.available_resources >= count) {
+        printf("\tRecursos sufiecientes, consumiendo...\n");
+
+        printf("%d - (!) Recurso tomado.\n", tid);
+        monitor.available_resources -= count;
+
+        sleep(2);
+    }
+
+    if (monitor.next_count > 0)
+        sem_post(&(monitor.next));
+    else
+        sem_post(&(monitor.mutex));
+}
+
+void* increase_count (int count) {
+    pid_t tid = syscall(SYS_gettid);
+    printf("\t(%d) Iniciando increase_count.\n", tid);
+
+    sem_wait(&(monitor.mutex));
+
+    printf("%d - Holaaa, ya use el recurso.\n", tid);
+    monitor.available_resources += count;
+
+    if (monitor.next_count > 0) {
+        printf("---- Next ----\n");
+        sem_post(&monitor.next);
+    } else {
+        printf("---- Mutex ----\n");
+        sem_post(&(monitor.mutex));
+    }
+}
+
+void* consume (void* arg) {
+    int i;
+
+    for (i = 0; i < nciclos; i++) {
+        printf("\tIniciando iteracion #%d.\n", i + 1);
+
+        pid_t tid = syscall(SYS_gettid);
+        int num = rand() % 5 + 1;
+        printf("\t(%d) Se consumiran %d recursos.\n", tid, num);
+
+        wait();
+        decrease_count(num);
+        increase_count(num);
+        signal();
+    }
+}
 
 int main () {
     printf("Iniciando programa.\n");
@@ -30,7 +129,7 @@ int main () {
             if (pthread_create(&threads[i], NULL, consume, NULL) != 0) {
                 fprintf (stderr, "Error: No se puede crear thread # %d\n", i);
             } else {
-                printf("Iniciando thread %d\n", (int)threads[i]);
+                printf("Iniciando thread %d\n", i + 1);
             }
         }
 
@@ -43,66 +142,4 @@ int main () {
     }
 
     return 0;
-}
-
-int initialize_monitor () {
-    int return_value = 1;
-
-    monitor.available_resources = maxRecursos;
-
-    if ((sem_init(&(monitor.mutex), 0, 1) == 0) && (sem_init(&(monitor.next), 0, 0) == 0))
-        return_value = 0;
-    else
-        printf("Unable to initialize semaphores\n");
-
-    return return_value;
-}
-
-void* consume (void* arg) {
-    int i;
-
-    for (i = 0; i < nciclos; i++) {
-        printf("\tIniciando iteracion #%d.\n", i + 1);
-
-        int num = rand() % 5 + 1;
-        printf("\tSe consumiran %d recursos.\n", num);
-
-        decrease_count(num);
-        increase_count(num);
-    }
-}
-
-void decrease_count (int count) {
-    printf("\tIniciando decrease_count.\n");
-
-    sem_wait(&(monitor.mutex));
-    printf("\tMutex adquirido, entrando a monitor.\n");
-
-    if (monitor.available_resources >= count) {
-        printf("\tRecursos sufiecientes, consumiendo...\n");
-
-        printf("%d - (!) Recurso tomado.\n", (int)pthread_self());
-        monitor.available_resources -= count;
-
-        sleep(2);
-    }
-
-    if (monitor.next_count > 0)
-        sem_post(&(monitor.next));
-    else
-        sem_post(&(monitor.mutex));
-}
-
-void increase_count (int count) {
-    printf("\tIniciando increase_count.\n");
-
-    sem_wait(&(monitor.mutex));
-
-    printf("%d - Holaaa, ya use el recurso.\n", (int)pthread_self());
-    monitor.available_resources += count;
-
-    if (monitor.next_count > 0)
-        sem_post(&monitor.next);
-    else
-        sem_post(&(monitor.mutex));
 }
--- Lab6-Semaforo.c
+++ Lab6-Semaforo.c
@@ -2,6 +2,8 @@
 # include <pthread.h>
 # include <semaphore.h> 
 # include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
 # include <sys/syscall.h>
 
 #define nthreads 5
@@ -9,57 +11,115 @@
 
 sem_t mutex;
 int available_resources = 1;
+char txtContent[2048];
+FILE * fp;
 
 void* doNothing (void* arg) {
 
     int j;
     pid_t tid;
+    tid = syscall(SYS_gettid);
+
+    char mes[50] = {0};
+    char t[10] = {0};
+    sprintf(t, "%d", tid);
+    strcat(mes, t);
+    strcat(mes, " - Semaforo abierto con exito.\n");
+    strcat(txtContent, mes);
+
     printf("%d - Semaforo abierto con exito.\n", tid);
 
     for (j = 0; j < nciclos; j++) {
         sem_wait(&mutex);
 
-        printf("\tIniciando iteracion %d\n", j + 1);
+        printf("Iniciando iteracion %d\n", j + 1);
+        char mes[50] = {0};
+        char t[5] = {0};
+        sprintf(t, "%d", j + 1);
+        strcat(mes, "Iniciando iteracion ");
+        strcat(mes, t);
+        strcat(mes, "\n");
+        strcat(txtContent, mes);
+
         printf("\t%d - (!) Recurso tomado.\n", tid);
+        char mes2[50] = {0};
+        char t2[10] = {0};
+        sprintf(t2, "%d", tid);
+        strcat(mes2, "\t");
+        strcat(mes2, t2);
+        strcat(mes2, " - (!) Recurso tomado.\n");
+        strcat(txtContent, mes2);
+        
         available_resources--;
 
         int num = rand() % 3 + 1;
         sleep(num);
 
         printf("\t%d - Holaaa, ya use el recurso.\n", tid);
+        char mes3[50] = {0};
+        strcat(mes3, "\t");
+        strcat(mes3, t2);
+        strcat(mes3, " - Holaaa, ya use el recurso.\n");
+        strcat(txtContent, mes3);
+
         available_resources++;
 
         sem_post(&mutex);
 
         printf("\t%d - Ya devolvi el recurso :)\n", tid);
+        char mes4[50] = {0};
+        strcat(mes4, "\t");
+        strcat(mes4, t2);
+        strcat(mes4, " - Ya devolvi el recurso :)\n");
+        strcat(txtContent, mes4);
     }
 }
 
+void* writeFile (char message[]) {
+    fprintf(fp, "%s", message);
+}
+
 int main() {
+    fp = fopen("SemaforoBitacora.txt", "w");
+
     printf("Iniciando programa.\n");
+    strcat(txtContent, "Iniciando programa.\n");
+
     pthread_t threads[nthreads];
     void * retvals[nthreads];
     int i;
 
-    sem_init(&mutex, 0, 1);
+    sem_init(&mutex, 0, 2);
 
     printf("Creando threads.\n");
+    strcat(txtContent, "Creando threads.\n");
     for (i = 0; i < nthreads; i++) {
         if (pthread_create(&threads[i], NULL, doNothing, NULL) != 0) {
             fprintf (stderr, "Error: No se puede crear thread # %d\n", i);
         } else {
-            printf("Iniciando thread %d\n", (int)threads[i]);
+            char s[5] = {0};
+            char m[50] = {0};
+            sprintf(s, "%d", i + 1);
+            strcat(m, "Iniciando thread ");
+            strcat(m, s);
+            strcat(m, "\n");
+
+            printf("Iniciando thread %d\n", i + 1);
+            strcat(txtContent, m);
         }
     }
 
     printf("Esperando threads.\n");
+    strcat(txtContent, "Esperando threads.\n");
     for (i = 0; i < nthreads; i++) {
         if (pthread_join(threads[i], &retvals[i]) != 0) {
             fprintf (stderr, "Error: No se puede hacer join al thread #%d\n", i);
         }
     }
 
-    sem_destroy(&mutex);
+    writeFile(txtContent);
 
+    sem_destroy(&mutex);
+    fclose(fp);
     return 0;
 }
